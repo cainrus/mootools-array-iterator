9e3cef71c9da77f4bc082acc0fb5ab09d9f653c9
cainrus <cainrus@gmail.com>
2010-10-28 12:14:21 +0300

    working on pass option + some refactoring

--- Source/Array.iterator.js
+++ Source/Array.iterator.js
@@ -57,10 +57,11 @@ var Iterator = new Class({
         // Allows to limit movement when reaches the edges
         limits      : false,
         // Allows to ignore indexes not in range
-        min: -1,
-        max: -1
+        min: null,
+        max: null,
         // TODO:
         // 1. option pass: array, keys to pass.
+        pass:[]
     },
     // Constructor
     initialize: function(ref, options){
@@ -80,6 +81,8 @@ var Iterator = new Class({
             var uid = getUid(), key = this.valid(Util.getData(uid));
             return Util.setData(uid, key);
         };
+
+        this.range(); // Set valid min and max
     },
     // key validator
     valid: function(){
@@ -92,8 +95,10 @@ var Iterator = new Class({
         if (key < 0) key = this.ref().length+key;
         // limits checks
         if (key<0 || key > length-1) return null;
+        // pass range checks
+        if (this.options.pass.indexOf(key)>-1) return null;
         // complex range checks
-        var range = function(side){ return (side < 0) ? null : side.limit(0, length);}
+        var range = function(side){ return (Number.from(side) === null) ? null : side.limit(0, length);}
         var min = range(this.options.min), max = range(this.options.max);
         if ((min && key < min)||(max && key > max)) return null;
         return key;
@@ -122,23 +127,23 @@ var Iterator = new Class({
     },
     // Return selected array value
     current: function(){
-        var key = arguments[0] || this.key();
+        var key = this.valid(arguments[0]) || this.key();
         return (key === null) ? null : this.ref()[key];
     },
-    // Move with offset back or forward
-    slide: function(offset){
-        var range = this.range(), key = this.key(); offset = Number.from(offset);
-        var limit = this.options.limits, pit = this.options.pit;
+    // Move with offset back or forward [,from index]
+    slide: function(offset, index){
+        var range = this.range(), key = [index,this.key()].pick();
+        var limit = this.options.limits, pit = this.options.pit, pass = this.options.pass;
         // Exit with null result if range or offset is invalid .
-        if (!range.length||offset===null) { this.jump(null); return null; }
+        if (!range.length||offset===null) return this.jump(null);
         if (offset===0) return this.current();
         // Pit option setup.
         if (pit) range.unshift(null);// Add null to map of indexes if pit is enabled
         // Move cursor from not existing index (null)
-        if (pit) key = (key === null) ? 0 : range.indexOf(String.from(key));
-        else if (offset>0) key = (key === null) ? (offset--,0) :range.indexOf(String.from(key));
-          else
-              key = (key === null) ? 0 : range.indexOf(String.from(key));
+        var index = range.indexOf(String.from(key));
+        if (pit) key = (key === null) ? 0 : index;
+        else if (offset>0) key = (key === null) ? (offset--,0) :index;
+          else key = (key === null) ? 0 : index;
        // Post process by limit option.
        if (!limit && offset.abs() >= range.length) {offset = offset%range.length;}
        key = key + offset;
@@ -147,20 +152,23 @@ var Iterator = new Class({
            if (key > max) key = max;
            if (key < 0) key = 0;
        } else {
-           if (key > max) key = 0 + (key-max-1);
-           if (key < 0) key =  max-(0-key-1);
+           if (key > max) key = key-max-1;
+           if (key < 0) key =  key+max+1;
        }
-       key = range[key];
-       this.jump(key);
-       return Number.from(this.ref()[key]);
+       key = range[key]; this.jump(key);
+       return this.current();
     },
     // Return range
     range: function(){
-        var array = this.ref(), length = array.length-1;
-        if (length<0) return []; // empty array ~ empty range
-        var min = this.options.min.limit(0, length);
-        var max = this.options.max.limit(0,length) || length;
-        return Object.keys(array).slice(min, max+1);
+        var array = this.ref(), length = array.length-1, keys = Object.keys(array);
+        with(this.options){
+            if (length<0) {min=max=null;return [];} // empty array ~ empty range
+            min = Number.from(this.options.min), max = Number.from(this.options.max);
+            if (min===null) min = 0; else min = min.limit(0, length);
+            if (max===null) max = length; else max = max.limit(0, length);
+            range = (min>max) ? keys.slice(min).combine(keys.slice(0,max+1)) : keys.slice(min, max+1);
+            return range.filter(function(index){return !!pass.indexOf(index);});
+        }
     }
 });
 
